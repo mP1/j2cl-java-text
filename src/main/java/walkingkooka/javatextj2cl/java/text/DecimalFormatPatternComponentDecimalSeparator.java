8ebff767a72c780e8db2e96bec90dadab7b9083e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-01-29 20:49:07 +1100

    DecimalFormatPatternComponentDecimalSeparator.LITERAL fix

--- src/main/java/walkingkooka/javatextj2cl/java/text/DecimalFormatPatternComponentDecimalSeparator.java
+++ src/main/java/walkingkooka/javatextj2cl/java/text/DecimalFormatPatternComponentDecimalSeparator.java
@@ -41,7 +41,7 @@ final class DecimalFormatPatternComponentDecimalSeparator extends DecimalFormatP
         return 0;
     }
 
-    final static char LITERAL = ',';
+    final static char LITERAL = '.';
 
     @Override
     public String toString() {
