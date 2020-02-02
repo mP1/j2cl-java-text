09711d5582c237b6fc4bde3713da4ff5d6df8f9e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-02-02 20:54:02 +1100

    DecimalFormatPatternParserPrefixSuffix

---
+++ src/main/java/walkingkooka/javatextj2cl/java/text/DecimalFormatPatternParserPrefixSuffix.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.javatextj2cl.java.text;
+
+/**
+ * A parser that handles parsing the prefix or suffix with a pattern.
+ */
+final class DecimalFormatPatternParserPrefixSuffix extends DecimalFormatPatternParser {
+
+    static DecimalFormatPatternParserPrefixSuffix with(final DecimalFormatPatternParserTextCursor text,
+                                                       final boolean stop) {
+        return new DecimalFormatPatternParserPrefixSuffix(text, stop);
+    }
+
+    private DecimalFormatPatternParserPrefixSuffix(final DecimalFormatPatternParserTextCursor text,
+                                                   final boolean stop) {
+        super(text);
+        this.stop = stop;
+    }
+
+    @Override
+    void handle(final char c) {
+        switch (c) {
+            case DecimalFormat.CURRENCY:
+                this.currency();
+                break;
+            case DecimalFormat.DECIMAL_SEPARATOR:
+            case DecimalFormat.DIGIT:
+            case DecimalFormat.DIGIT_ZERO:
+            case DecimalFormat.GROUPING_SEPARATOR:
+                if (this.stop) {
+                    this.shouldStop = true;
+                } else {
+                    this.addCharacterLiteral(c);
+                }
+                break;
+            case DecimalFormat.MINUS_SIGN:
+                this.minusSign();
+                break;
+            case DecimalFormat.PERCENT:
+                this.percent();
+                break;
+            case DecimalFormat.PER_MILLE:
+                this.perMille();
+                break;
+            default:
+                this.addCharacterLiteral(c);
+                break;
+        }
+    }
+
+    private final boolean stop;
+
+    @Override
+    boolean shouldStop() {
+        return this.shouldStop;
+    }
+
+    private boolean shouldStop;
+}
---
+++ src/test/java/walkingkooka/javatextj2cl/java/text/DecimalFormatPatternParserTestCase.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.javatextj2cl.java.text;
+
+import walkingkooka.InvalidCharacterException;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.text.CharSequences;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public abstract class DecimalFormatPatternParserTestCase<P extends DecimalFormatPatternParser> extends DecimalFormatPatternTestCase<P> {
+
+    DecimalFormatPatternParserTestCase() {
+        super();
+    }
+
+    final void parseAndCheck(final String text,
+                             final DecimalFormatPatternComponent... components) {
+        this.parseAndCheck(this.createParser(text, 0),
+                "",
+                components);
+    }
+
+    final void parseAndCheck(final P parser,
+                             final DecimalFormatPatternComponent... components) {
+        this.parseAndCheck(parser,
+                "",
+                components);
+    }
+
+    final void parseAndCheck(final String text,
+                             final String left,
+                             final DecimalFormatPatternComponent... components) {
+        final DecimalFormatPatternParserTextCursor cursor = DecimalFormatPatternParserTextCursor.with(text, 0);
+        final P parser = this.createParser(cursor);
+
+        assertEquals(Lists.of(components),
+                parser.parse(),
+                "components parse " + CharSequences.quoteAndEscape(text));
+
+        final StringBuilder actualLeft = new StringBuilder();
+        while (cursor.hasMore()) {
+            actualLeft.append(cursor.charAt());
+            cursor.next();
+        }
+
+        assertEquals(left,
+                actualLeft.toString(),
+                () -> "remaining pattern, parse " + CharSequences.quoteAndEscape(text) + " components: " + Arrays.toString(components));
+    }
+
+    final void parseAndCheck(final P parser,
+                             final String left,
+                             final DecimalFormatPatternComponent... components) {
+        final DecimalFormatPatternParserTextCursor cursor = parser.pattern;
+
+        assertEquals(Lists.of(components),
+                parser.parse(),
+                "components parse " + CharSequences.quoteAndEscape(cursor.text));
+
+        final StringBuilder actualLeft = new StringBuilder();
+        while (cursor.hasMore()) {
+            actualLeft.append(cursor.charAt());
+            cursor.next();
+        }
+
+        assertEquals(left,
+                actualLeft.toString(),
+                () -> "remaining pattern, parse " + CharSequences.quoteAndEscape(cursor.text) + " components: " + Arrays.toString(components));
+    }
+
+    final void parseFails(final String pattern,
+                          final int position) {
+        this.parseFails(pattern,
+                0,
+                position);
+    }
+
+    final void parseFails(final String pattern,
+                          final int offset,
+                          final int position) {
+        this.parseFails(this.createParser(pattern, offset),
+                position);
+    }
+
+    final void parseFails(final P parser,
+                          final int position) {
+        assertThrows(IllegalArgumentException.class,
+                () -> new java.text.DecimalFormat());
+        final InvalidCharacterException thrown = assertThrows(InvalidCharacterException.class,
+                () -> parser.parse());
+        assertEquals(position, thrown.position(), "position");
+        assertEquals(parser.pattern.text, thrown.text(), "text");
+    }
+
+    // helpers..........................................................................................................
+
+    final P createParser(final String pattern) {
+        return this.createParser(pattern, 0);
+    }
+
+    final P createParser(final String pattern,
+                         final int position) {
+        return this.createParser(DecimalFormatPatternParserTextCursor.with(pattern, position));
+    }
+
+    abstract P createParser(final DecimalFormatPatternParserTextCursor pattern);
+
+
+    final void checkPrefixAndSuffix(final String pattern,
+                                    final String prefix,
+                                    final String suffix) {
+        this.checkPositivePrefixAndSuffix(pattern, prefix, suffix);
+        this.checkNegativePrefixAndSuffix(pattern, prefix, suffix);
+    }
+
+    final void checkPositivePrefixAndSuffix(final String pattern,
+                                            final String positivePrefix,
+                                            final String positiveSuffix) {
+        this.checkPositivePrefix(pattern, positivePrefix);
+        this.checkPositiveSuffix(pattern, positiveSuffix);
+    }
+
+    final void checkPositivePrefix(final String pattern,
+                                   final String positivePrefix) {
+        final java.text.DecimalFormat jdk = new java.text.DecimalFormat(pattern);
+
+        assertEquals(positivePrefix, jdk.getPositivePrefix(), "positivePrefix");
+    }
+
+    final void checkPositiveSuffix(final String pattern,
+                                   final String positiveSuffix) {
+        final java.text.DecimalFormat jdk = new java.text.DecimalFormat(pattern);
+
+        assertEquals(positiveSuffix, jdk.getPositiveSuffix(), "positiveSuffix");
+    }
+
+    final void checkNegativePrefixAndSuffix(final String pattern,
+                                            final String negativePrefix,
+                                            final String negativeSuffix) {
+        this.checkNegativePrefix(pattern, negativePrefix);
+        this.checkNegativeSuffix(pattern, negativeSuffix);
+    }
+
+    final void checkNegativePrefix(final String pattern,
+                                   final String negativePrefix) {
+        final java.text.DecimalFormat jdk = new java.text.DecimalFormat(pattern);
+
+        assertEquals(negativePrefix, jdk.getNegativePrefix(), "negativePrefix");
+    }
+
+    final void checkNegativeSuffix(final String pattern,
+                                   final String negativeSuffix) {
+        final java.text.DecimalFormat jdk = new java.text.DecimalFormat(pattern);
+
+        assertEquals(negativeSuffix, jdk.getNegativeSuffix(), "negativeSuffix");
+    }
+
+    // TypeNameTesting..................................................................................................
+
+    @Override
+    public final String typeNamePrefix() {
+        return DecimalFormatPatternParser.class.getSimpleName();
+    }
+
+    @Override
+    public final String typeNameSuffix() {
+        return "";
+    }
+}
