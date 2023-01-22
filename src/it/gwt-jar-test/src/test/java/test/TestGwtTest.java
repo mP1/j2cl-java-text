6c58453edbd1f85036e508b0bc2cf2a3f32e1fac
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-01-22 18:20:19 +1100

    Text.gwt.xml

---
+++ src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package test;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class TestGwtTest extends GWTTestCase {
+    @Override
+    public String getModuleName() {
+        return "test.Test";
+    }
+
+    public void testAssertEquals() {
+        assertEquals(
+                1,
+                1
+        );
+    }
+
+    public void testDecimalFormat() throws Exception {
+        final DecimalFormat decimalFormat = (DecimalFormat) NumberFormat.getNumberInstance(
+                Locale.forLanguageTag("EN-AU")
+        );
+
+        assertEquals(
+                12.5,
+                decimalFormat.format(12.49)
+        );
+    }
+
+    public void testSimpleDateFormatParse() throws Exception {
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd",
+                Locale.forLanguageTag("EN-AU")
+        );
+
+        Date date = simpleDateFormat.parse("2022-01-22");
+
+        assertEquals(
+                new Date(1922, 1, 22),
+                date
+        );
+    }
+}
